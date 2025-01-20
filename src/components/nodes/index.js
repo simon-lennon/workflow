9199cd8211439c49541149eb0242f302dbaf9418
simon-lennon <142030358+simon-lennon@users.noreply.github.com>
2025-01-20 17:22:13 +0000

    Add node components index

---
+++ src/components/nodes/index.js
@@ -0,0 +1,7 @@
+export { default as ApprovalNode } from './ApprovalNode.vue'
+export { default as ReviewNode } from './ReviewNode.vue'
+export { default as TodoNode } from './TodoNode.vue'
+export { default as NotificationNode } from './NotificationNode.vue'
+export { default as DocCollectionNode } from './DocCollectionNode.vue'
+export { default as SystemTaskNode } from './SystemTaskNode.vue'
+export { default as TaskCollectionNode } from './TaskCollectionNode.vue'
