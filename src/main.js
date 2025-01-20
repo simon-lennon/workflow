851c864d07da3777e0448e31236476003421bd92
simon-lennon <142030358+simon-lennon@users.noreply.github.com>
2025-01-20 17:01:55 +0000

    Fix Bootstrap imports in main.js

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
-// Import Bootstrap and its styles
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
+// Import Bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 // Import Vue Flow styles
 import '@vue-flow/core/dist/style.css'
 import '@vue-flow/core/dist/theme-default.css'
 
 const app = createApp(App)
-app.use(createPinia())
+const pinia = createPinia()
+
+app.use(pinia)
 app.mount('#app')
