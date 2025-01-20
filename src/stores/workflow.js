b374c83be48a8a09e868a8fde11ee08c6d54d55e
simon-lennon <142030358+simon-lennon@users.noreply.github.com>
2025-01-20 17:11:23 +0000

    Update workflow store to support approval nodes

--- src/stores/workflow.js
+++ src/stores/workflow.js
@@ -12,9 +12,7 @@ export const useStore = defineStore('workflow', {
       
       const baseNode = {
         id: `${type}-${this.nodeCount}`,
-        type: 'default',
         position,
-        class: `node-${type}`,
         draggable: true,
         connectable: true,
         selectable: true,
@@ -24,17 +22,33 @@ export const useStore = defineStore('workflow', {
         case 'start':
           return {
             ...baseNode,
+            type: 'default',
+            class: 'node-start',
             data: { label: 'Start' },
           }
         case 'task':
           return {
             ...baseNode,
+            type: 'default',
+            class: 'node-task',
             data: { label: `Task ${this.nodeCount}` },
           }
         case 'condition':
           return {
             ...baseNode,
+            type: 'default',
+            class: 'node-condition',
             data: { label: `Condition ${this.nodeCount}` },
+          }
+        case 'approval':
+          return {
+            ...baseNode,
+            type: 'approval',
+            data: {
+              label: `Approval ${this.nodeCount}`,
+              approver: '',
+              level: '1'
+            },
           }
         default:
           return baseNode
